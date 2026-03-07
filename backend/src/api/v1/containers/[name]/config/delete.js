bc849f76d3f9d6f3c7ff768e3a89a86a7be2b155
Hugo Chilemme <hugo.chilemme@gmail.com>
2026-03-07 17:43:43 +0100

    Remove outdated documentation comments from delete.js

--- backend/src/api/v1/containers/[name]/config/delete.js
+++ backend/src/api/v1/containers/[name]/config/delete.js
@@ -1,27 +1,6 @@
 import incus from "../../../../../../lib/modules/incus.js";
 
 
-/*
-Supprimer une clé de configuration d'un container
-Endpoint
-
-DELETE /api/v1/containers/:name/config
-Description Supprime une ou plusieurs clés de configuration d'un container spécifique.
-
-Body parameters
-
-Paramètre	Description
-keys	Tableau de clés de configuration à supprimer - ex: [ "environment.VAR1", "limits.cpu" ]
-Fonctionnalités
-
- supprimer une ou plusieurs clés de configuration d'un container
-Response
-
-{
-  "status": true,
-}
-*/
-
 export default async function del(req, res) {
 	const { _cName } = req;
 	const { keys } = req.body;

2d9be225e7ff731f693197379736cd503227f31f
Hugo Chilemme <hugo.chilemme@gmail.com>
2026-03-08 19:55:14 +0100

    Remove exec function for executing commands inside a container

--- backend/lib/modules/incus/containers.js
+++ backend/lib/modules/incus/containers.js
@@ -208,13 +208,3 @@ export const updateDevices = async (name, devices) => {
 	return execute(`${COMMAND} ${deviceArgs}`);
 };
 
-
-/**
- * Execute a command inside a container.
- * @param {string} name - Container name.
- * @param {string} command - Command to execute inside the container.
- * @returns {Promise<any>} Command output.
- */
-export const exec = async (name, command) => {
-	return await execute(`${COMMAND} exec ${name} -- ${command}`);
-};

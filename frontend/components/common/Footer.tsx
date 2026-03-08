f365e931f996677f388e108c564f75072e089621
Hugo Chilemme <hugo.chilemme@gmail.com>
2026-03-08 21:51:38 +0100

    feat: add Footer component for consistent page structure

---
+++ frontend/components/common/Footer.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function Header()
+{
+	return (
+		<div className="w-full p-4 py-6 border-t border-neutral-800">
+			<div className="max-w-7xl mx-auto text-gray-400 text-sm">
+				Incus Orbit. Created by Hugo Chilemme
+			</div>
+		</div>
+	)
+}
